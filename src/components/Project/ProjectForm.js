a1fa8c493160d3bc1584fd79a7f3a73fddbef5e5
Cristiano <cristianodevsystem@gmail.com>
2022-03-10 09:00:10 -0300

    :rocket: Estrutura da page do form do projeto

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -1,22 +1,27 @@
-function ProjectForm() {
+import Input from "../form/input/Input";
+import Select from "../form/select/Select";
+import SubmitButton from "../form/submit/SubmitButton";
+import select from "./ProjectForm.module.css";
+
+function ProjectForm({btnText}) {
   return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto"></input>
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento do total"></input>
-      </div>
-      <div>
-        <select name="category_id">
-          <option disabled select>
-            Selecioner uma categoria
-          </option>
-        </select>
-      </div>
-      <div>
-        <input type="submit" value="Criar projeto" />
-      </div>
+    <form className={select.form}>
+      <Input
+        type="text"
+        text="Nome do Projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+
+      <Input
+        type="number"
+        text="Orçamento do Projeto"
+        name="baduget"
+        placeholder="Insira o orçamento total"
+      />
+      <Select name="category_id" text="Selecione uma categoria" />
+
+      <SubmitButton text={btnText} />
     </form>
   );
 }
--- src/components/pages/NewProject/NewProject.js
+++ src/components/pages/NewProject/NewProject.js
@@ -4,9 +4,9 @@ import styles from "./NewProject.module.css";
 function NewProject() {
   return (
     <div className={styles.newproject_container}>
-      <h1> Criar Projeto </h1>
-      <p> Crie seu projeto para depois adionar os serviços </p>
-      <ProjectForm />
+      <h1> Criar Projeto </h1>{" "}
+      <p> Crie seu projeto para depois adionar os serviços </p>{" "}
+      <ProjectForm btnText="Criar projeto" />
     </div>
   );
 }
